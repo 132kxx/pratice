41ba6a8dc8bcd5ad501f125e3d96069976b9c561
kxx <132kxx@gmail.com>
2023-01-15 21:50:18 +0900

    Initial Commit

---
+++ pratice1111/pratice1111App.swift
@@ -0,0 +1,17 @@
+//
+//  pratice1111App.swift
+//  pratice1111
+//
+//  Created by kxx: on 2023/01/15.
+//
+
+import SwiftUI
+
+@main
+struct pratice1111App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
