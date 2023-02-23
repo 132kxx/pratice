ed929662fbb15d8b32373e8b92235b3e4f9a3c58
kxx <132kxx@gmail.com>
2023-02-23 23:46:51 +0900

    ListView

--- pratice1111/Bootcamp/ListView.swift
+++ pratice1111/Bootcamp/ListView.swift
@@ -13,17 +13,66 @@ struct ListView: View {
         "apple", "orange", "banana"
     ]
     
+    @State var veggies: [String] = [
+        "tomato", "potato", "carrot"
+    ]
+    
     var body: some View {
-        List {
-            Section {
-                ForEach(fruits, id: \.self) { fruit in
-                    Text(fruit)
+        NavigationStack {
+            List {
+                Section {
+                    ForEach(fruits, id: \.self) { fruit in
+                        Text(fruit)
+                    }
+                    .onDelete(perform: delete)
+                    .onMove(perform: move)
+                } header: {
+                    Text("fruit")
                 }
-            } header: {
-                Text("fruit")
-            }
+                
+                Section {
+                    ForEach(veggies, id: \.self) { veggies in
+                        Text(veggies.capitalized)
+                    }
+                } header: {
+                    Text("Veggies")
+                }
 
+                
+            }
+            .listStyle(SidebarListStyle())
+            .navigationTitle("Grocery")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    addButton
+                }
+            }
+            .tint(.red)
         }
+    }
+    
+    var addButton: some View {
+        Button {
+            add()
+        } label: {
+            Text("Add")
+        }
+    }
+        
+    func delete(indexSet: IndexSet) {
+        fruits.remove(atOffsets: indexSet)
+    }
+    
+    func move(index: IndexSet, newOffset: Int) {
+        fruits.move(fromOffsets: index, toOffset: newOffset)
+    }
+    
+    func add() {
+        fruits.append("Coconut")
     }
 }
 
